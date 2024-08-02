6a1849e8e37e9885c4c1bef7cbec9a4fd0e57b70
jtorova5 <jtorova5@gmail.com>
2024-08-02 06:56:00 -0500

    feat: se agregan los atributos de la clase Driver

--- Models/Driver.cs
+++ Models/Driver.cs
@@ -7,5 +7,7 @@ namespace simulacro.Models;
 
 public class Driver
 {
-
+    public string LicenseNunber { get; set; }
+    public string LicenseCategory { get; set; }
+    public int DrivingExperience { get; set; }
 }
