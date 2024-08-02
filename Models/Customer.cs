73d42a81644b6e6371ad85b3e0612f077f716d6e
jtorova5 <jtorova5@gmail.com>
2024-08-02 07:06:41 -0500

    feat: creación del método constructor de la clase Customer

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 
 namespace simulacro.Models;
 
-public class Customer
+public class Customer : User
 {
     public string MembershipLevel { get; set; }
     public string PreferredPaymentMethod { get; set; }
+
+    public Customer(string name, string lastName, string typeDocument, string identificationNumber, DateOnly birthDate, string email, string phoneNumber, string address, string membershipLevel, string preferredPaymentMethod) : base(name, lastName, typeDocument, identificationNumber, birthDate, email, phoneNumber, address)
+    {
+        MembershipLevel = membershipLevel;
+        PreferredPaymentMethod = preferredPaymentMethod;
+    }
 }
