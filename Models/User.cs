1a38afce82c1afc40f4be334919b9a319fc43c7f
jtorova5 <jtorova5@gmail.com>
2024-08-02 06:44:19 -0500

    feat: se agregan los atributos de la clase User

--- Models/User.cs
+++ Models/User.cs
@@ -7,5 +7,15 @@ namespace simulacro.Models;
 
 public class User
 {
-    
+    protected Guid Id { get; set; }
+    protected string Name { get; set; }
+    protected string LastName { get; set; }
+    protected string TypeDocument { get; set; }
+    protected string IdentificationNumber { get; set; }
+    protected DateOnly BirthDate { get; set; }
+    protected string Email { get; set; }
+    protected string PhoneNumber { get; set; }
+    protected string Address { get; set; }
+
+
 }
