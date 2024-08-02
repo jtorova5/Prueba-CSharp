f1c7008e27789a11a83dad2736ad079d6298380a
jtorova5 <jtorova5@gmail.com>
2024-08-02 07:12:49 -0500

    feat: se agregan los atributos de la clase Vehicle

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -7,5 +7,11 @@ namespace simulacro.Models;
 
 public class Vehicle
 {
-
+    public int Id { get; set; }
+    public string CarPlate { get; set; }
+    public string Type { get; set; }
+    public string EngineNumber { get; set; }
+    public string SerialNumber { get; set; }
+    public byte PeopleCapacity { get; set; }
+    public Driver Owner { get; set; }
 }
