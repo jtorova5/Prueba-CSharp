b28ef90a0b3ccc4d96fbc601c313c3770c9e3ae2
jtorova5 <jtorova5@gmail.com>
2024-08-02 08:13:33 -0500

    feat: se crean métodos para agregar y eliminar Drivers, Customers y Vehicles

--- Models/Company.cs
+++ Models/Company.cs
@@ -10,4 +10,34 @@ public class Company
     public static List<Driver> drivers = new List<Driver>();
     public static List<Customer> customers = new List<Customer>();
     public static List<Vehicle> vehicles = new List<Vehicle>();
+
+    public static void AddVehicle(Vehicle vehicle)
+    {
+        vehicles.Add(vehicle);
+    }
+
+    public static void RemoveVehicle(Vehicle vehicle)
+    {
+        vehicles.Remove(vehicle);
+    }
+
+    public static void AddDriver(Driver driver)
+    {
+        drivers.Add(driver);
+    }
+
+    public static void RemoveDriver(Driver driver)
+    {
+        drivers.Remove(driver);
+    }
+
+    public static void AddCustomer(Customer customer)
+    {
+        customers.Add(customer);
+    }
+
+    public static void RemoveCustomer(Customer customer)
+    {
+        customers.Remove(customer);
+    }
 }
